1c7c4582bfbb1a22467bc2c6a126e2764a30825f
Serhii <mathodcoast@gmail.com>
2021-07-13 16:48:02 +0300

    GP-115 update completed

--- 1-0-rdbms-and-sql/1-2-3-many-to-many-schema/src/main/resources/db/migration/table_initialization.sql
+++ 1-0-rdbms-and-sql/1-2-3-many-to-many-schema/src/main/resources/db/migration/table_initialization.sql
@@ -1,4 +1,3 @@
-
 /*
 WallStreet database should store information about brokers, sales groups and its relations.
 Each broker must have a unique username. First and last names are also mandatory.
@@ -18,4 +17,30 @@ A sales group can consists of more than one broker, while each broker can be ass
 - All alternative keys (unique) should be named according to the following rule "UQ_table_name_column_name"
 */
 
--- TODO: write SQL script to create a database tables according to the requirements
+CREATE TABLE IF NOT EXISTS broker (
+    id         BIGINT,
+    username   VARCHAR(255) NOT NULL,
+    first_name VARCHAR(255) NOT NULL,
+    last_name  VARCHAR(255) NOT NULL,
+    CONSTRAINT PK_broker PRIMARY KEY (id),
+    CONSTRAINT UQ_broker_username UNIQUE (username)
+    );
+
+
+CREATE TABLE IF NOT EXISTS sales_group (
+    id                     BIGINT,
+    name                   VARCHAR(255) NOT NULL,
+    transaction_type       VARCHAR(255) NOT NULL,
+    max_transaction_amount INT          NOT NULL,
+    CONSTRAINT PK_sales_group PRIMARY KEY (id),
+    CONSTRAINT UQ_sales_group_name UNIQUE (name)
+    );
+
+
+CREATE TABLE IF NOT EXISTS broker_sales_group (
+    broker_id      BIGINT NOT NULL,
+    sales_group_id BIGINT NOT NULL,
+    CONSTRAINT PK_broker_sales_group PRIMARY KEY (broker_id, sales_group_id),
+    CONSTRAINT FK_broker_sales_group_broker FOREIGN KEY (broker_id) REFERENCES broker,
+    CONSTRAINT FK_broker_sales_group_sales_group FOREIGN KEY (sales_group_id) REFERENCES sales_group
+    );
