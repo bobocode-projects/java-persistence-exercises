de138cd371bf6b5490a3394fe98268366113c4ec
Serhii <mathodcoast@gmail.com>
2021-07-20 16:36:34 +0300

    GP-115 fix the description of wall street db initializer task

--- 1-0-rdbms-and-sql/1-2-3-many-to-many-schema/src/main/resources/db/migration/table_initialization.sql
+++ 1-0-rdbms-and-sql/1-2-3-many-to-many-schema/src/main/resources/db/migration/table_initialization.sql
@@ -1,11 +1,15 @@
-
 /*
+
 WallStreet database should store information about brokers, sales groups and its relations.
+
 Each broker must have a unique username. First and last names are also mandatory.
+
 A sales group is a special group that has its own restrictions. Sale groups are used to organize the work of brokers.
 Each group mush have a unique name, transaction type (string), and max transaction amount (a number). All field are
 mandatory.
+
 A sales group can consists of more than one broker, while each broker can be associated with more than one sale group.
+
   TECH NOTES AND NAMING CONVENTION
 - All tables, columns and constraints are named using "snake case" naming convention
 - All table names must be singular (e.g. "user", not "users")
@@ -13,9 +17,11 @@ A sales group can consists of more than one broker, while each broker can be ass
 - Link tables should have composite primary key, that consists of two other foreign key columns
 - All primary key, foreign key, and unique constraint should be named according to the naming convention.
 - All link tables should have a composite key that consists of two foreign key columns
+
 - All primary keys should be named according to the following rule "PK_table_name"
 - All foreign keys should be named according to the following rule "FK_table_name_reference_table_name"
 - All alternative keys (unique) should be named according to the following rule "UQ_table_name_column_name"
+
 */
 
 -- TODO: write SQL script to create a database tables according to the requirements
