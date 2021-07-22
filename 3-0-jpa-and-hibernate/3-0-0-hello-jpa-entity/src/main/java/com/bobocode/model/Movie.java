520cad305df746372d62197da1cccb79c37acb9e
Serhii <mathodcoast@gmail.com>
2021-07-22 11:20:54 +0300

    GP-123 add completed solution

--- 3-0-jpa-and-hibernate/3-0-0-hello-jpa-entity/src/main/java/com/bobocode/model/Movie.java
+++ 3-0-jpa-and-hibernate/3-0-0-hello-jpa-entity/src/main/java/com/bobocode/model/Movie.java
@@ -4,6 +4,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.*;
+
 /**
  * TODO: you're job is to implement mapping for JPA entity {@link Movie}
  * - specify id
@@ -14,12 +16,19 @@ import lombok.Setter;
 @NoArgsConstructor
 @Getter
 @Setter
+@Entity
+@Table(name = "movie")
 public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "name", nullable = false)
     private String name;
 
+    @Column(name = "director", nullable = false)
     private String director;
 
+    @Column(name = "duration")
     private Integer durationSeconds;
 }
