8374c2803108257823192c1f65708513ffe4f05a
Serhii <mathodcoast@gmail.com>
2021-07-23 12:49:24 +0300

    GP-125 update the tests

--- 3-0-jpa-and-hibernate/3-0-2-query-helper/src/test/java/com/bobocode/QueryHelperTest.java
+++ 3-0-jpa-and-hibernate/3-0-2-query-helper/src/test/java/com/bobocode/QueryHelperTest.java
@@ -4,18 +4,15 @@ import com.bobocode.exception.QueryHelperException;
 import com.bobocode.model.Account;
 import com.bobocode.util.EntityManagerUtil;
 import com.bobocode.util.TestDataGenerator;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.fail;
 
-
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class QueryHelperTest {
     private static EntityManagerFactory entityManagerFactory;
     private static EntityManagerUtil emUtil;
@@ -34,25 +31,48 @@ public class QueryHelperTest {
     }
 
     @Test
-    public void testQueryHelperReturnsResult() {
+    @Order(1)
+    @DisplayName("Query helper returns a result")
+    public void queryHelperReturnsResult() {
         Account account = saveRandomAccount();
         Long accountId = account.getId();
 
         Account foundAccount = queryHelper.readWithinTx(entityManager -> entityManager.find(Account.class, accountId));
 
-        assertThat(foundAccount, notNullValue());
+        assertThat(foundAccount).isNotNull();
     }
 
     @Test
-    public void testQueryHelperUsesReadOnly() {
+    @Order(2)
+    @DisplayName("Query helper uses \"Read Only\"")
+    public void queryHelperUsesReadOnly() {
         Account account = saveRandomAccount();
         Long accountId = account.getId();
 
         tryToUpdateFirstName(accountId);
         Account foundAccount = queryHelper.readWithinTx(entityManager -> entityManager.find(Account.class, accountId));
 
-        assertThat(foundAccount.getFirstName(), not(equalTo("XXX")));
-        assertThat(foundAccount.getFirstName(), equalTo(account.getFirstName()));
+        assertThat(foundAccount.getFirstName()).isNotEqualTo("XXX");
+        assertThat(foundAccount.getFirstName()).isEqualTo(account.getFirstName());
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName("Query helper throws exception")
+    public void queryHelperThrowsException() {
+        Account account = TestDataGenerator.generateAccount();
+        emUtil.performWithinTx(entityManager -> entityManager.persist(account));
+        try {
+            queryHelper.readWithinTx(entityManager -> {
+                Account managedAccount = entityManager.find(Account.class, account.getId());
+                throwException();
+                return managedAccount;
+            });
+            fail("Exception should be thrown");
+        } catch (Exception e) {
+            assertThat(e.getClass()).isEqualTo(QueryHelperException.class);
+            assertThat(e.getMessage()).contains("Transaction is rolled back");
+        }
     }
 
     private Account saveRandomAccount() {
@@ -67,24 +87,6 @@ public class QueryHelperTest {
             managedAccount.setFirstName("XXX");
             return managedAccount;
         });
-    }
-
-
-    @Test
-    public void testQueryHelperThrowsException() {
-        Account account = TestDataGenerator.generateAccount();
-        emUtil.performWithinTx(entityManager -> entityManager.persist(account));
-        try {
-            queryHelper.readWithinTx(entityManager -> {
-                Account managedAccount = entityManager.find(Account.class, account.getId());
-                throwException();
-                return managedAccount;
-            });
-            fail("Exception should be thrown");
-        } catch (Exception e) {
-            assertThat(e.getClass(), equalTo(QueryHelperException.class));
-            assertThat(e.getMessage(), containsString("Transaction is rolled back"));
-        }
     }
 
     private void throwException() {
