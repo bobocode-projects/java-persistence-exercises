41eaa4b9cc3191cc45ee6dfb99750eaf9c932b6b
Serhii Hryhus <mathodcoast@gmail.com>
2021-07-27 18:28:58 +0300

    GP-126 remove completed solution

--- 3-0-jpa-and-hibernate/3-0-3-account-dao/src/main/java/com/bobocode/dao/AccountDaoImpl.java
+++ 3-0-jpa-and-hibernate/3-0-3-account-dao/src/main/java/com/bobocode/dao/AccountDaoImpl.java
@@ -1,14 +1,10 @@
 package com.bobocode.dao;
 
-import com.bobocode.exception.AccountDaoException;
 import com.bobocode.model.Account;
+import com.bobocode.util.ExerciseNotCompletedException;
 
-import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
-import javax.persistence.TypedQuery;
 import java.util.List;
-import java.util.function.Consumer;
-import java.util.function.Function;
 
 public class AccountDaoImpl implements AccountDao {
     private EntityManagerFactory emf;
@@ -19,65 +15,32 @@ public class AccountDaoImpl implements AccountDao {
 
     @Override
     public void save(Account account) {
-        performWithinPersistenceContext(em -> em.persist(account));
+        throw new ExerciseNotCompletedException(); // todo
     }
 
     @Override
     public Account findById(Long id) {
-        return performReturningWithinPersistenceContext(entityManager -> entityManager.find(Account.class, id));
+        throw new ExerciseNotCompletedException(); // todo
     }
 
     @Override
     public Account findByEmail(String email) {
-        return performReturningWithinPersistenceContext(entityManager -> {
-            TypedQuery<Account> findByEmailQuery
-                    = entityManager.createQuery("select a from Account a where a.email = :email", Account.class);
-            findByEmailQuery.setParameter("email", email);
-            return findByEmailQuery.getSingleResult();
-        });
+        throw new ExerciseNotCompletedException(); // todo
     }
 
     @Override
     public List<Account> findAll() {
-        return performReturningWithinPersistenceContext(entityManager ->
-                entityManager.createQuery("select a from Account a", Account.class).getResultList());
+        throw new ExerciseNotCompletedException(); // todo
     }
 
     @Override
     public void update(Account account) {
-        performWithinPersistenceContext(em -> em.merge(account));
+        throw new ExerciseNotCompletedException(); // todo
     }
 
     @Override
     public void remove(Account account) {
-        performWithinPersistenceContext(entityManager -> {
-            Account mergedAccount = entityManager.merge(account);
-            entityManager.remove(mergedAccount);
-        });
-    }
-
-    private void performWithinPersistenceContext(Consumer<EntityManager> entityManagerConsumer) {
-        performReturningWithinPersistenceContext(entityManager -> {
-            entityManagerConsumer.accept(entityManager);
-            return null;
-        });
-    }
-
-    private <T> T performReturningWithinPersistenceContext(Function<EntityManager, T> entityManagerFunction) {
-        EntityManager entityManager = emf.createEntityManager();
-        entityManager.getTransaction().begin();
-        T result;
-        try {
-            result = entityManagerFunction.apply(entityManager);
-            entityManager.getTransaction().commit();
-        } catch (Exception e) {
-            entityManager.getTransaction().rollback();
-            throw new AccountDaoException("Error performing dao operation. Transaction is rolled back!", e);
-        } finally {
-            entityManager.close();
-        }
-        return result;
+        throw new ExerciseNotCompletedException(); // todo
     }
 }
-
 
