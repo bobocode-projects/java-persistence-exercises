b6ddd631c885dc757dc612b331b16bcb1d0ddc37
Serhii Hryhus <mathodcoast@gmail.com>
2021-07-27 18:26:16 +0300

    GP-126 update the tests

--- 3-0-jpa-and-hibernate/3-0-3-account-dao/src/test/java/com/bobocode/dao/AccountDaoTest.java
+++ 3-0-jpa-and-hibernate/3-0-3-account-dao/src/test/java/com/bobocode/dao/AccountDaoTest.java
@@ -4,9 +4,7 @@ import com.bobocode.exception.AccountDaoException;
 import com.bobocode.model.Account;
 import com.bobocode.util.TestDataGenerator;
 import org.hibernate.Session;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -19,15 +17,11 @@ import java.sql.ResultSet;
 import java.sql.Timestamp;
 import java.util.List;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.everyItem;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.isIn;
-import static org.hamcrest.Matchers.notNullValue;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
-
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class AccountDaoTest {
     private static EntityManagerFactory emf;
     private static AccountDao accountDao;
@@ -44,15 +38,127 @@ class AccountDaoTest {
     }
 
     @Test
-    void testSaveAccount() {
+    @Order(1)
+    @DisplayName("Save account")
+    void saveAccount() {
         Account account = TestDataGenerator.generateAccount();
 
         accountDao.save(account);
         boolean saved = isSaved(account);
 
-        assertThat(account.getId(), notNullValue());
-        assertThat(saved, is(true));
+        assertThat(account.getId()).isNotNull();
+        assertThat(saved).isTrue();
+    }
 
+    @Test
+    @Order(2)
+    @DisplayName("Save throws exception when account is invalid")
+    void testSaveInvalidAccount() {
+        Account invalidAccount = TestDataGenerator.generateAccount();
+        invalidAccount.setEmail(null);
+        assertThatThrownBy(() -> {
+            accountDao.save(invalidAccount);
+        }).isInstanceOf(AccountDaoException.class);
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName("Find account by Id")
+    void testFindAccountById() {
+        Account account = TestDataGenerator.generateAccount();
+        saveTestAccount(account);
+
+        Account foundAccount = accountDao.findById(account.getId());
+
+        assertThat(account).isEqualTo(foundAccount);
+    }
+
+    @Test
+    @Order(4)
+    @DisplayName("Find account by email")
+    void testFindAccountByEmail() {
+        Account account = TestDataGenerator.generateAccount();
+        saveTestAccount(account);
+
+        Account foundAccount = accountDao.findByEmail(account.getEmail());
+
+        assertThat(account).isEqualTo(foundAccount);
+    }
+
+    @Test
+    @Order(5)
+    @DisplayName("Find all accounts")
+    void testFindAllAccounts() {
+        List<Account> accounts = TestDataGenerator.generateAccountList(3);
+        accounts.forEach(this::saveTestAccount);
+
+        List<Account> foundAccounts = accountDao.findAll();
+
+        assertThat(foundAccounts).containsAll(accounts);
+    }
+
+    @Test
+    @Order(6)
+    @DisplayName("Update accounts")
+    void testUpdateAccount() {
+        Account account = TestDataGenerator.generateAccount();
+        saveTestAccount(account);
+
+        account.setBalance(account.getBalance().add(BigDecimal.valueOf(1000).setScale(2, RoundingMode.HALF_UP)));
+        accountDao.update(account);
+        boolean balanceUpdated = isBalanceUpdated(account);
+
+        assertThat(balanceUpdated).isTrue();
+    }
+
+    @Test
+    @Order(7)
+    @DisplayName("Update throws exception when account is invalid")
+    void testUpdateInvalidAccount() {
+        Account account = TestDataGenerator.generateAccount();
+        saveTestAccount(account);
+        account.setFirstName(null);
+        assertThatThrownBy(() -> {
+            accountDao.update(account);
+        }).isInstanceOf(AccountDaoException.class);
+    }
+
+    @Test
+    @Order(8)
+    @DisplayName("Remove account")
+    void testRemoveAccount() {
+        Account account = TestDataGenerator.generateAccount();
+        saveTestAccount(account);
+
+        accountDao.remove(account);
+        boolean saved = isSaved(account);
+
+        assertThat(saved).isFalse();
+    }
+
+    private boolean isBalanceUpdated(Account account) {
+        EntityManager entityManager = emf.createEntityManager();
+        boolean isUpdated = entityManager.unwrap(Session.class).doReturningWork(connection -> {
+            PreparedStatement statement = connection.prepareStatement("SELECT balance = ? FROM account WHERE id = ?");
+            statement.setBigDecimal(1, account.getBalance());
+            statement.setLong(2, account.getId());
+            ResultSet resultSet = statement.executeQuery();
+            resultSet.next();
+            return resultSet.getBoolean(1);
+        });
+        entityManager.close();
+        return isUpdated;
+    }
+
+    private boolean isSaved(Account account) {
+        EntityManager entityManager = emf.createEntityManager();
+        boolean isSaved = entityManager.unwrap(Session.class).doReturningWork(connection -> {
+            PreparedStatement statement = connection.prepareStatement("SELECT * FROM account WHERE id = ?");
+            statement.setLong(1, account.getId());
+            return statement.executeQuery().next();
+        });
+        entityManager.close();
+        return isSaved;
     }
 
     private void saveTestAccount(Account account) {
@@ -76,110 +182,4 @@ class AccountDaoTest {
         });
         entityManager.close();
     }
-
-    private boolean isSaved(Account account) {
-        EntityManager entityManager = emf.createEntityManager();
-        boolean isSaved = entityManager.unwrap(Session.class).doReturningWork(connection -> {
-            PreparedStatement statement = connection.prepareStatement("SELECT * FROM account WHERE id = ?");
-            statement.setLong(1, account.getId());
-            return statement.executeQuery().next();
-        });
-        entityManager.close();
-        return isSaved;
-    }
-
-    @Test
-    void testFindAccountById() {
-        Account account = TestDataGenerator.generateAccount();
-        saveTestAccount(account);
-
-        Account foundAccount = accountDao.findById(account.getId());
-
-        assertEquals(account, foundAccount);
-    }
-
-    @Test
-    void testFindAccountByEmail() {
-        Account account = TestDataGenerator.generateAccount();
-        saveTestAccount(account);
-
-        Account foundAccount = accountDao.findByEmail(account.getEmail());
-
-        assertEquals(account, foundAccount);
-    }
-
-    @Test
-    void testFindAllAccounts() {
-        List<Account> accounts = TestDataGenerator.generateAccountList(3);
-        accounts.forEach(this::saveTestAccount);
-
-        List<Account> foundAccounts = accountDao.findAll();
-
-        assertThat(accounts, everyItem(isIn(foundAccounts)));
-    }
-
-    @Test
-    void testUpdateAccount() {
-        Account account = TestDataGenerator.generateAccount();
-        saveTestAccount(account);
-
-        account.setBalance(account.getBalance().add(BigDecimal.valueOf(1000).setScale(2, RoundingMode.HALF_UP)));
-        accountDao.update(account);
-        boolean balanceUpdated = isBalanceUpdated(account);
-
-        assertThat(balanceUpdated, is(true));
-    }
-
-    private boolean isBalanceUpdated(Account account) {
-        EntityManager entityManager = emf.createEntityManager();
-        boolean isUpdated = entityManager.unwrap(Session.class).doReturningWork(connection -> {
-            PreparedStatement statement = connection.prepareStatement("SELECT balance = ? FROM account WHERE id = ?");
-            statement.setBigDecimal(1, account.getBalance());
-            statement.setLong(2, account.getId());
-            ResultSet resultSet = statement.executeQuery();
-            resultSet.next();
-            return resultSet.getBoolean(1);
-        });
-        entityManager.close();
-        return isUpdated;
-    }
-
-    @Test
-    void testRemoveAccount() {
-        Account account = TestDataGenerator.generateAccount();
-        saveTestAccount(account);
-
-        accountDao.remove(account);
-        boolean saved = isSaved(account);
-
-        assertThat(saved, is(false));
-    }
-
-    @Test
-    void testSaveInvalidAccount() {
-        try {
-            Account invalidAccount = TestDataGenerator.generateAccount();
-            invalidAccount.setEmail(null);
-            accountDao.save(invalidAccount);
-            fail("AccountDaoException should be thrown");
-        } catch (Exception e) {
-            assertEquals(AccountDaoException.class, e.getClass());
-        }
-    }
-
-    @Test
-    void testUpdateInvalidAccount() {
-        try {
-            Account account = TestDataGenerator.generateAccount();
-            saveTestAccount(account);
-
-            account.setFirstName(null);
-            accountDao.update(account);
-
-            fail("AccountDaoException should be thrown");
-        } catch (Exception e) {
-            assertEquals(AccountDaoException.class, e.getClass());
-        }
-    }
-
 }
