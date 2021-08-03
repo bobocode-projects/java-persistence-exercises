34c2b06104eaa8c132ff1525ea81c218a416c8e2
Serhii Hryhus <mathodcoast@gmail.com>
2021-08-03 18:55:50 +0300

    GP-127 upgrade tests

--- 3-0-jpa-and-hibernate/3-1-1-employee-profile/src/test/java/com/bobocode/EmployeeProfileMappingTest.java
+++ 3-0-jpa-and-hibernate/3-1-1-employee-profile/src/test/java/com/bobocode/EmployeeProfileMappingTest.java
@@ -4,23 +4,17 @@ import com.bobocode.model.Employee;
 import com.bobocode.model.EmployeeProfile;
 import com.bobocode.util.EntityManagerUtil;
 import org.apache.commons.lang3.RandomStringUtils;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.JoinColumn;
-import javax.persistence.Persistence;
-import javax.persistence.PersistenceException;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.lang.reflect.Field;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.notNullValue;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class EmployeeProfileMappingTest {
     private static EntityManagerUtil emUtil;
     private static EntityManagerFactory entityManagerFactory;
@@ -37,80 +31,95 @@ class EmployeeProfileMappingTest {
     }
 
     @Test
-    void testSaveEmployeeOnly() {
+    @Order(1)
+    @DisplayName("The employee table has correct name")
+    void employeeTableHasCorrectName() {
+        Table table = Employee.class.getAnnotation(Table.class);
+        String tableName = table.name();
+
+        assertThat(tableName, equalTo("employee"));
+    }
+
+    @Test
+    @Order(2)
+    @DisplayName("Save an employee only")
+    void saveEmployeeOnly() {
         Employee employee = createRandomEmployee();
 
         emUtil.performWithinTx(entityManager -> entityManager.persist(employee));
 
-        assertThat(employee.getId(), notNullValue());
-    }
-
-    private Employee createRandomEmployee() {
-        Employee employee = new Employee();
-        employee.setEmail(RandomStringUtils.randomAlphabetic(15));
-        employee.setFistName(RandomStringUtils.randomAlphabetic(15));
-        employee.setLastName(RandomStringUtils.randomAlphabetic(15));
-        return employee;
+        assertThat(employee.getId()).isNotNull();
     }
 
     @Test
-    void testSaveEmployeeWithoutEmail() {
+    @Order(3)
+    @DisplayName("Save an employee without email")
+    void saveEmployeeWithoutEmail() {
         Employee employee = createRandomEmployee();
         employee.setEmail(null);
-        try {
-            emUtil.performWithinTx(entityManager -> entityManager.persist(employee));
-            fail("Exception should be thrown");
-        } catch (Exception e) {
-            assertTrue(e instanceof PersistenceException);
-        }
+
+        assertThatExceptionOfType(PersistenceException.class).isThrownBy(() ->
+                emUtil.performWithinTx(entityManager -> entityManager.persist(employee)));
     }
 
     @Test
-    void testSaveEmployeeFirstName() {
+    @Order(4)
+    @DisplayName("Save an employee without first name")
+    void saveEmployeeWithoutFirstName() {
         Employee employee = createRandomEmployee();
         employee.setFistName(null);
-        try {
-            emUtil.performWithinTx(entityManager -> entityManager.persist(employee));
-            fail("Exception should be thrown");
-        } catch (Exception e) {
-            assertTrue(e instanceof PersistenceException);
-        }
+
+        assertThatExceptionOfType(PersistenceException.class).isThrownBy(() ->
+                emUtil.performWithinTx(entityManager -> entityManager.persist(employee)));
     }
 
     @Test
-    void testSaveEmployeeLastName() {
+    @Order(5)
+    @DisplayName("Save an employee without last name")
+    void testSaveEmployeeWithoutLastName() {
         Employee employee = createRandomEmployee();
         employee.setLastName(null);
-        try {
-            emUtil.performWithinTx(entityManager -> entityManager.persist(employee));
-            fail("Exception should be thrown");
-        } catch (Exception e) {
-            assertTrue(e instanceof PersistenceException);
-        }
+
+        assertThatExceptionOfType(PersistenceException.class).isThrownBy(() ->
+                emUtil.performWithinTx(entityManager -> entityManager.persist(employee)));
     }
 
     @Test
-    void testSaveEmployeeProfileOnly() {
+    @Order(6)
+    @DisplayName("The employee profile table has correct name")
+    void employeeProfileTableHasCorrectName() {
+        Table table = EmployeeProfile.class.getAnnotation(Table.class);
+        String tableName = table.name();
+
+        assertThat(tableName).isEqualTo("employee_profile");
+    }
+
+    @Test
+    @Order(7)
+    @DisplayName("Save only a profile of employee")
+    void saveEmployeeProfileOnly() {
         EmployeeProfile employeeProfile = createRandomEmployeeProfile();
         employeeProfile.setId(666L);
 
-        try {
-            emUtil.performWithinTx(entityManager -> entityManager.persist(employeeProfile));
-            fail("Exception should be thrown");
-        } catch (Exception e) {
-            assertTrue(e instanceof PersistenceException);
-        }
-    }
-
-    private EmployeeProfile createRandomEmployeeProfile() {
-        EmployeeProfile employeeProfile = new EmployeeProfile();
-        employeeProfile.setDepartment(RandomStringUtils.randomAlphabetic(15));
-        employeeProfile.setPosition(RandomStringUtils.randomAlphabetic(15));
-        return employeeProfile;
+        assertThatExceptionOfType(PersistenceException.class).isThrownBy(() ->
+                emUtil.performWithinTx(entityManager -> entityManager.persist(employeeProfile)));
     }
 
     @Test
-    void testSaveBothEmployeeAndEmployeeProfile() {
+    @Order(8)
+    @DisplayName("The foreign key column has a correct name")
+    void foreignKeyColumnHasCorrectName() throws NoSuchFieldException {
+        Field employee = EmployeeProfile.class.getDeclaredField("employee");
+        JoinColumn joinColumn = employee.getAnnotation(JoinColumn.class);
+        String foreignKeyColumnName = joinColumn.name();
+
+        assertThat(foreignKeyColumnName).isEqualTo("employee_id");
+    }
+
+    @Test
+    @Order(9)
+    @DisplayName("Save both employee and employee profile")
+    void saveBothEmployeeAndEmployeeProfile() {
         Employee employee = createRandomEmployee();
         EmployeeProfile employeeProfile = createRandomEmployeeProfile();
 
@@ -120,13 +129,15 @@ class EmployeeProfileMappingTest {
             entityManager.persist(employeeProfile);
         });
 
-        assertThat(employee.getId(), notNullValue());
-        assertThat(employeeProfile.getId(), notNullValue());
-        assertThat(employeeProfile.getId(), equalTo(employee.getId()));
+        assertThat(employee.getId()).isNotNull();
+        assertThat(employeeProfile.getId()).isNotNull();
+        assertThat(employeeProfile.getId()).isEqualTo(employee.getId());
     }
 
     @Test
-    void testAddEmployeeProfile() {
+    @Order(10)
+    @DisplayName("Add an employee profile")
+    void addEmployeeProfile() {
         Employee employee = createRandomEmployee();
         emUtil.performWithinTx(entityManager -> entityManager.persist(employee));
         long employeeId = employee.getId();
@@ -138,73 +149,61 @@ class EmployeeProfileMappingTest {
             entityManager.persist(employeeProfile);
         });
 
-        assertThat(employee.getId(), notNullValue());
-        assertThat(employeeProfile.getId(), notNullValue());
-        assertThat(employeeProfile.getId(), equalTo(employee.getId()));
+        assertThat(employee.getId()).isNotNull();
+        assertThat(employeeProfile.getId()).isNotNull();
+        assertThat(employeeProfile.getId()).isEqualTo(employee.getId());
     }
 
     @Test
-    void testAddEmployeeWithoutPosition() {
+    @Order(11)
+    @DisplayName("Add employee profile without position")
+    void addEmployeeWithoutPosition() {
         Employee employee = createRandomEmployee();
         emUtil.performWithinTx(entityManager -> entityManager.persist(employee));
         long employeeId = employee.getId();
 
         EmployeeProfile profileWithoutPosition = createRandomEmployeeProfile();
         profileWithoutPosition.setPosition(null);
-        try {
-            emUtil.performWithinTx(entityManager -> {
-                Employee managedEmployee = entityManager.find(Employee.class, employeeId);
-                profileWithoutPosition.setEmployee(managedEmployee);
-                entityManager.persist(profileWithoutPosition);
-            });
-            fail("Exception should be thrown");
-        } catch (Exception e) {
-            assertTrue(e instanceof PersistenceException);
-        }
+
+        assertThatExceptionOfType(PersistenceException.class).isThrownBy(() ->
+                emUtil.performWithinTx(entityManager -> {
+                    Employee managedEmployee = entityManager.find(Employee.class, employeeId);
+                    profileWithoutPosition.setEmployee(managedEmployee);
+                    entityManager.persist(profileWithoutPosition);
+                }));
     }
 
     @Test
-    void testAddEmployeeWithoutDepartment() {
+    @Order(12)
+    @DisplayName("Add employee profile without department")
+    void addEmployeeWithoutDepartment() {
         Employee employee = createRandomEmployee();
         emUtil.performWithinTx(entityManager -> entityManager.persist(employee));
         long employeeId = employee.getId();
 
         EmployeeProfile profileWithoutDepartment = createRandomEmployeeProfile();
         profileWithoutDepartment.setDepartment(null);
-        try {
-            emUtil.performWithinTx(entityManager -> {
-                Employee managedEmployee = entityManager.find(Employee.class, employeeId);
-                profileWithoutDepartment.setEmployee(managedEmployee);
-                entityManager.persist(profileWithoutDepartment);
-            });
-            fail("Exception should be thrown");
-        } catch (Exception e) {
-            assertTrue(e instanceof PersistenceException);
-        }
+
+        assertThatExceptionOfType(PersistenceException.class).isThrownBy(() ->
+                emUtil.performWithinTx(entityManager -> {
+                    Employee managedEmployee = entityManager.find(Employee.class, employeeId);
+                    profileWithoutDepartment.setEmployee(managedEmployee);
+                    entityManager.persist(profileWithoutDepartment);
+                }));
     }
 
-    @Test
-    void testForeignKeyColumnHasCorrectName() throws NoSuchFieldException {
-        Field employee = EmployeeProfile.class.getDeclaredField("employee");
-        JoinColumn joinColumn = employee.getAnnotation(JoinColumn.class);
-        String foreignKeyColumnName = joinColumn.name();
-
-        assertThat(foreignKeyColumnName, equalTo("employee_id"));
+    private Employee createRandomEmployee() {
+        Employee employee = new Employee();
+        employee.setEmail(RandomStringUtils.randomAlphabetic(15));
+        employee.setFistName(RandomStringUtils.randomAlphabetic(15));
+        employee.setLastName(RandomStringUtils.randomAlphabetic(15));
+        return employee;
     }
 
-    @Test
-    void testEmployeeTableHasCorrectName() {
-        Table table = Employee.class.getAnnotation(Table.class);
-        String tableName = table.name();
-
-        assertThat(tableName, equalTo("employee"));
-    }
-
-    @Test
-    void testEmployeeProfileTableHasCorrectName() {
-        Table table = EmployeeProfile.class.getAnnotation(Table.class);
-        String tableName = table.name();
-
-        assertThat(tableName, equalTo("employee_profile"));
+    private EmployeeProfile createRandomEmployeeProfile() {
+        EmployeeProfile employeeProfile = new EmployeeProfile();
+        employeeProfile.setDepartment(RandomStringUtils.randomAlphabetic(15));
+        employeeProfile.setPosition(RandomStringUtils.randomAlphabetic(15));
+        return employeeProfile;
     }
 }
