8577ef24bc1e52ce0f654776ea5c024bda601ead
Serhii Hryhus <mathodcoast@gmail.com>
2021-08-04 19:34:12 +0300

    GP-129 remove completed solution

--- 3-0-jpa-and-hibernate/3-1-3-author-book/src/main/java/com/bobocode/model/Author.java
+++ 3-0-jpa-and-hibernate/3-1-3-author-book/src/main/java/com/bobocode/model/Author.java
@@ -1,62 +1,48 @@
 package com.bobocode.model;
 
-import lombok.AccessLevel;
+import com.bobocode.util.ExerciseNotCompletedException;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
 import java.util.HashSet;
-import java.util.Objects;
 import java.util.Set;
 
+/**
+ * todo:
+ * - implement hashCode() that return constant value 31
+ * - make setter for field {@link Author#books} private
+ * - initialize field {@link Author#books} as new {@link HashSet}
+ * - implement a helper {@link Author#addBook(Book)} that establishes a relation on both sides
+ * - implement a helper {@link Author#removeBook(Book)} that drops a relation on both sides
+ * <p>
+ * - configure JPA entity
+ * - specify table name: "author"
+ * - configure auto generated identifier
+ * - configure mandatory column "first_name" for field {@link Author#firstName}
+ * - configure mandatory column "last_name" for field {@link Author#lastName}
+ * <p>
+ * - configure many-to-many relation between {@link Author} and {@link Book}
+ * - configure cascade operations for this relations {@link CascadeType#PERSIST} and {@link CascadeType#MERGE}
+ * - configure link (join) table "author_book"
+ * - configure foreign key column "book_id" references book table
+ * - configure foreign key column "author_id" references author table
+ */
 @NoArgsConstructor
 @Getter
 @Setter
-@Entity
-@Table(name = "author")
 public class Author {
-    @Id
-    @GeneratedValue
     private Long id;
-
-    @Column(name = "first_name", nullable = false)
     private String firstName;
-
-    @Column(name = "last_name", nullable = false)
     private String lastName;
-
-    @Setter(AccessLevel.PRIVATE)
-    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
-    @JoinTable(name = "author_book",
-            joinColumns = @JoinColumn(name = "author_id"),
-            inverseJoinColumns = @JoinColumn(name = "book_id")
-    )
-    private Set<Book> books = new HashSet<>();
+    private Set<Book> books;
 
     public void addBook(Book book) {
-        books.add(book);
-        book.getAuthors().add(this);
+        throw new ExerciseNotCompletedException();
     }
 
     public void removeBook(Book book) {
-        books.remove(book);
-        book.getAuthors().remove(this);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Author)) return false;
-
-        Author author = (Author) o;
-
-        return Objects.equals(id, author.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return 31;
+        throw new ExerciseNotCompletedException();
     }
 }
-
--- 3-0-jpa-and-hibernate/3-1-3-author-book/src/main/java/com/bobocode/model/Book.java
+++ 3-0-jpa-and-hibernate/3-1-3-author-book/src/main/java/com/bobocode/model/Book.java
@@ -1,32 +1,32 @@
 package com.bobocode.model;
 
-import lombok.*;
-import org.hibernate.annotations.NaturalId;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * todo:
+ * - implement equals() and hashCode() based on {@link Book#isbn}
+ * - make setter for field {@link Book#authors} private
+ * - initialize field {@link Book#authors} as new {@link HashSet}
+ * <p>
+ * - configure JPA entity
+ * - specify table name: "book"
+ * - configure auto generated identifier
+ * - configure mandatory column "name" for field {@link Book#name}
+ * - configure mandatory unique column "isbn" for field {@link Book#isbn}, it is a natural key candidate
+ * <p>
+ * - configure many-to-many relation as mapped on the {@link Author} side
+ */
 @NoArgsConstructor
 @Getter
 @Setter
-@EqualsAndHashCode(of = "isbn")
-@Entity
-@Table(name = "book")
 public class Book {
-    @Id
-    @GeneratedValue
     private Long id;
-
-    @Column(nullable = false)
     private String name;
-
-    @NaturalId
-    @Column(nullable = false, unique = true)
     private String isbn;
-
-    @Setter(AccessLevel.PRIVATE)
-    @ManyToMany(mappedBy = "books")
-    private Set<Author> authors = new HashSet<>();
+    private Set<Author> authors;
 }
-
