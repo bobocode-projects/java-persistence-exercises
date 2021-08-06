d6315f3435f778e4404556ee90f0b7ad87342f9a
Serhii Hryhus <mathodcoast@gmail.com>
2021-08-06 09:54:01 +0300

    GP-127 add completed solution

--- 3-0-jpa-and-hibernate/3-1-1-employee-profile/src/main/java/com/bobocode/model/Employee.java
+++ 3-0-jpa-and-hibernate/3-1-1-employee-profile/src/main/java/com/bobocode/model/Employee.java
@@ -4,6 +4,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.*;
+
 /**
  * todo:
  * - configure JPA entity
@@ -16,9 +18,19 @@ import lombok.Setter;
 @NoArgsConstructor
 @Getter
 @Setter
+@Entity
+@Table(name = "employee")
 public class Employee {
+    @Id
+    @GeneratedValue
     private Long id;
+
+    @Column(nullable = false)
     private String email;
+
+    @Column(nullable = false)
     private String fistName;
+
+    @Column(nullable = false)
     private String lastName;
 }
