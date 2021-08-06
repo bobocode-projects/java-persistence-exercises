85f1ea268378ba3a4ff51f2acc7d5ccd99e0c026
Serhii Hryhus <mathodcoast@gmail.com>
2021-08-06 09:54:01 +0300

    GP-127 add completed solution

--- 3-0-jpa-and-hibernate/3-1-1-employee-profile/src/main/java/com/bobocode/model/EmployeeProfile.java
+++ 3-0-jpa-and-hibernate/3-1-1-employee-profile/src/main/java/com/bobocode/model/EmployeeProfile.java
@@ -4,6 +4,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.*;
+
 /**
  * todo:
  * - configure JPA entity
@@ -16,9 +18,20 @@ import lombok.Setter;
 @NoArgsConstructor
 @Getter
 @Setter
+@Entity
+@Table(name = "employee_profile")
 public class EmployeeProfile {
+    @Id
     private Long id;
+
+    @MapsId
+    @OneToOne
+    @JoinColumn(name = "employee_id")
     private Employee employee;
+
+    @Column(nullable = false)
     private String position;
+
+    @Column(nullable = false)
     private String department;
 }
