84b60e08992f039529053f4faa899c3c129f2bbe
Serhii Hryhus <mathodcoast@gmail.com>
2021-08-17 13:24:30 +0300

    GP-129 add completed solution

--- 3-0-jpa-and-hibernate/3-1-3-author-book/src/main/java/com/bobocode/model/Book.java
+++ 3-0-jpa-and-hibernate/3-1-3-author-book/src/main/java/com/bobocode/model/Book.java
@@ -1,9 +1,9 @@
 package com.bobocode.model;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
+import org.hibernate.annotations.NaturalId;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -24,9 +24,22 @@ import java.util.Set;
 @NoArgsConstructor
 @Getter
 @Setter
+@EqualsAndHashCode(of = "isbn")
+@Entity
+@Table(name = "book")
 public class Book {
+    @Id
+    @GeneratedValue
     private Long id;
+
+    @Column(nullable = false)
     private String name;
+
+    @NaturalId
+    @Column(nullable = false, unique = true)
     private String isbn;
-    private Set<Author> authors;
+
+    @Setter(AccessLevel.PRIVATE)
+    @ManyToMany(mappedBy = "books")
+    private Set<Author> authors = new HashSet<>();
 }
