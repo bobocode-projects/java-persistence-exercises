e44bfeddb2e535362135f9340fdc4fde12dbea8c
Taras Boychuk <tarass.boychuk@gmail.com>
2022-08-18 19:15:12 +0300

    GP-157 Improve ProductDao Exercise * Revert Impl

--- 2-0-jdbc-api/2-1-1-product-dao/src/main/java/com/bobocode/dao/ProductDaoImpl.java
+++ 2-0-jdbc-api/2-1-1-product-dao/src/main/java/com/bobocode/dao/ProductDaoImpl.java
@@ -1,17 +1,10 @@
 package com.bobocode.dao;
 
-import com.bobocode.exception.DaoOperationException;
 import com.bobocode.model.Product;
-import lombok.SneakyThrows;
+import com.bobocode.util.ExerciseNotCompletedException;
 
 import javax.sql.DataSource;
-import java.sql.Date;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
-
-import static java.sql.Statement.RETURN_GENERATED_KEYS;
 
 public class ProductDaoImpl implements ProductDao {
     private DataSource dataSource;
@@ -22,111 +15,27 @@ public class ProductDaoImpl implements ProductDao {
 
     @Override
     public void save(Product product) {
-        try (var connection = dataSource.getConnection()) {
-            try (var insertStatement = connection.prepareStatement(
-                    "INSERT INTO products(name, producer, price, expiration_date) VALUES(?, ?, ?, ?)",
-                    RETURN_GENERATED_KEYS)
-            ) {
-                insertStatement.setString(1, product.getName());
-                insertStatement.setString(2, product.getProducer());
-                insertStatement.setBigDecimal(3, product.getPrice());
-                insertStatement.setDate(4, Date.valueOf(product.getExpirationDate()));
-                try {
-                    insertStatement.executeUpdate();
-                } catch (SQLException e) {
-                    throw new DaoOperationException("Error saving product: " + product, e);
-                }
-
-                var generatedIdResultSet = insertStatement.getGeneratedKeys();
-                generatedIdResultSet.next();
-                var id = generatedIdResultSet.getLong(1);
-                product.setId(id);
-            }
-        } catch (SQLException e) {
-            throw new DaoOperationException("Error saving product " + product, e);
-        }
+        throw new ExerciseNotCompletedException();// todo
     }
 
     @Override
     public List<Product> findAll() {
-        try (var connection = dataSource.getConnection()) {
-            try (var statement = connection.createStatement()) {
-                var productList = new ArrayList<Product>();
-                var rs = statement.executeQuery("select * from products");
-                while (rs.next()) {
-                    var product = createProductFromResultSet(rs);
-                    productList.add(product);
-                }
-                return productList;
-            }
-        } catch (SQLException e) {
-            throw new DaoOperationException("Error selecting all products", e);
-        }
-    }
-
-    @SneakyThrows
-    private Product createProductFromResultSet(ResultSet rs) {
-        var product = new Product();
-        product.setId(rs.getLong("id"));
-        product.setName(rs.getString("name"));
-        product.setProducer(rs.getString("producer"));
-        product.setPrice(rs.getBigDecimal("price"));
-        product.setExpirationDate(rs.getDate("expiration_date").toLocalDate());
-        product.setCreationTime(rs.getTimestamp("creation_time").toLocalDateTime());
-        return product;
+        throw new ExerciseNotCompletedException();// todo
     }
 
     @Override
     public Product findOne(Long id) {
-        try (var connection = dataSource.getConnection()) {
-            try (var statement = connection.createStatement()) {
-                var rs = statement.executeQuery("select * from products where id = " + id);
-                if (rs.next()) {
-                    return createProductFromResultSet(rs);
-                } else {
-                    throw new DaoOperationException("Product with id = " + id + " does not exist");
-                }
-            }
-        } catch (SQLException e) {
-            throw new DaoOperationException("Error selecting product by id = " + id, e);
-        }
+        throw new ExerciseNotCompletedException();// todo
     }
 
     @Override
     public void update(Product product) {
-        verifyProductId(product);
-        try (var connection = dataSource.getConnection()) {
-            try (var updateStatement = connection.prepareStatement("UPDATE products SET name = ?, producer = ?, price = ?, expiration_date = ? WHERE id = ?")) {
-                updateStatement.setString(1, product.getName());
-                updateStatement.setString(2, product.getProducer());
-                updateStatement.setBigDecimal(3, product.getPrice());
-                updateStatement.setDate(4, Date.valueOf(product.getExpirationDate()));
-                updateStatement.setLong(5, product.getId());
-
-                updateStatement.executeUpdate();
-            }
-        } catch (SQLException e) {
-            throw new DaoOperationException("Error updating product " + product, e);
-        }
-    }
-
-    private static void verifyProductId(Product product) {
-        if (product.getId() == null) {
-            throw new DaoOperationException("Product id cannot be null");
-        }
+        throw new ExerciseNotCompletedException();// todo
     }
 
     @Override
     public void remove(Product product) {
-        verifyProductId(product);
-        try (var connection = dataSource.getConnection()) {
-            try (var deleteStatement = connection.prepareStatement("delete from products where id = ?")) {
-                deleteStatement.setLong(1, product.getId());
-                deleteStatement.executeUpdate();
-            }
-        } catch (SQLException e) {
-            throw new DaoOperationException("Error removing product by id = " + product.getId(), e);
-        }
+        throw new ExerciseNotCompletedException();// todo
     }
 
 }
