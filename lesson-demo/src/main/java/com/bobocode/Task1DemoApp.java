3a75813aff8ba6234ecc11e07a257408dbadad34
Taras Boychuk <tboychuk@bobocode.com>
2023-12-28 07:22:11 +0200

    Task 1 – Insert 1000 notes

---
+++ lesson-demo/src/main/java/com/bobocode/Task1DemoApp.java
@@ -0,0 +1,31 @@
+package com.bobocode;
+
+import lombok.SneakyThrows;
+import org.postgresql.ds.PGSimpleDataSource;
+
+import javax.sql.DataSource;
+
+public class Task1DemoApp {
+    @SneakyThrows
+    public static void main(String[] args) {
+        DataSource dataSource = initDb();
+        try (var connection = dataSource.getConnection()) {
+            try (var insertStatement = connection.prepareStatement("insert into notes(title, body, person_id) values(?,?,?);")) {
+                var start = System.nanoTime();
+                // todo: insert 1000 notes
+                var end = System.nanoTime();
+                System.out.println((end - start) / 1000_000 + "ms");
+            }
+        }
+
+    }
+
+    private static DataSource initDb() {
+        var dataSource = new PGSimpleDataSource();
+        dataSource.setURL("jdbc:postgresql://localhost:5432/postgres");
+        dataSource.setUser("ju23user");
+        dataSource.setPassword("ju23pass");
+        return dataSource;
+    }
+
+}
